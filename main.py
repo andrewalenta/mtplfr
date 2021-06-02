8835df701ee69f162e763f3c0bb0131c56859d86
andrewalenta <andre.walenta@gmail.com>
2021-06-02 23:22:10 +0200

    prediction for glm added

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import sys
 import get_data as gd
 import ols_mtpl
 import glm
+import ols_logit as ols_l
 
 if __name__ == '__main__':
     df = gd.get_data()
     ols_mtpl.ols_mtpl(df)
     glm.glm(df)
+    ols_l.ols_logit(df)
     #test
 
---
+++ ols_logit.py
@@ -0,0 +1,37 @@
+from statsmodels.formula.api import logit
+import pandas as pd
+import statsmodels.api as sm
+#from statsmodels.formula.api import ols
+from sklearn.model_selection import train_test_split
+
+def ols_logit(df):
+    y = df['Frequency']
+    X = df.drop(columns=[
+        'Frequency'])  # ['VehPower', 'VehAge', 'DrivAge', 'BonusMalus', 'VehBrand', 'VehGas', 'Area', 'Density', 'Region']
+    X1 = df[['DrivAge', 'VehPower', 'VehAge', 'Density', 'BonusMalus']]
+    cat_data = ['VehBrand', 'VehGas', 'Area', 'Region']
+    num_data = ['VehPower', 'VehAge', 'DrivAge', 'BonusMalus', 'Density']
+
+    dum_Vehbrand = pd.get_dummies(X['VehBrand'])
+    dum_VehGas = pd.get_dummies(X['VehGas'])
+    dum_Area = pd.get_dummies(X['Area'])
+    dum_Region = pd.get_dummies(X['Region'])
+
+    X.append(['dum_Vehbrand', 'dum_VehGas', 'dum_Area', 'dum_Region'])
+    X = df.drop(columns=['VehBrand', 'VehGas', 'Area', 'Region'])
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+    mod = sm.OLS(y_train, X_train)  # Describe model
+
+    res = mod.fit()  # Fit model try
+    print(res.summary())
+
+    y_pred = res.predict(X_test)
+
+    print("")
+    print(y_pred)
+
+    X_ols = X.to_numpy()
+    freq_brand = logit("Frequency ~ VehPower", data=X_train).fit()
+
